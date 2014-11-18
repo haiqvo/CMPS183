0a84eaf7ffb1026ef80f104512034d701a697684
Hai Vo <haivo@outlook.com>
2014-11-17 20:15:44 -0800

    add a game play page

--- controllers/game_menu.py
+++ controllers/game_menu.py
@@ -16,5 +16,18 @@ def index():
 
 @auth.requires_login() 
 def join():
+	form=FORM(INPUT(_type='submit', _value='Yes'))
+	form.add_button('No', URL("game_menu","index"))
+	if form.accepts(request, session):
+		game = db.game(request.args(0,cast=int))
+		player_game = db((db.game_play.player_id == auth.user)&(db.game_play.game_id == game)).select(db.game_play.ALL).first()
+		if player_game is None:
+			db.game_play.insert(game_id=game, player_id=auth.user, amount_bet=0, win=False)
+		else:
+			player_game.update(game_id=game)	
+		redirect(URL("game_menu","game",args=[request.args(0,cast=int)]))
+	return dict(form=form)
 
-	return dict(message=T('testing'))
+def game():
+	game = db.game(request.args(0,cast=int)) or redirect(URL("game_menu", "index"))
+	return dict(message=T('testing'), game=game)
--- models/game.py
+++ models/game.py
@@ -6,10 +6,16 @@ db.define_table('game',
 	Field('date_started', 'datetime'),
 	Field('date_ended', 'datetime'))
 
+db.game.creator.requires = IS_IN_DB(db, db.auth_user.id)
+
 db.define_table('game_play',
 	Field('game_id', db.game),
+	Field('player_id', db.auth_user),
 	Field('amount_bet', 'integer'),
 	Field('win', 'boolean'))
+
+db.game_play.game_id.requires = IS_IN_DB(db, db.game.id)
+db.game_play.player_id.requires = IS_IN_DB(db, db.auth_user.id)
 
 db.define_table('user_status',
 	Field('user_id', db.auth_user),
